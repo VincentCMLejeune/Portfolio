d8c5fc84b5297e0e2e339adab6218c4dae0c167d
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 21:14:45 +0100

    Created flipcards from previous work template

--- portfolio/src/components/projectcard/ProjectCard.jsx
+++ portfolio/src/components/projectcard/ProjectCard.jsx
@@ -5,10 +5,20 @@ import styles from "./ProjectCard.module.css";
 export default function ProjectCard(props) {
   return (
     <Link to={props.link} style={{ textDecoration: "none" }}>
-      <div className={styles.projectCard}>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        <img src={props.img} alt={props.title} className={styles.cardImg} />
+      <div className={styles.flipCard}>
+        <div className={styles.flipCardInner}>
+          <div className={styles.flipCardFront}>
+            <img
+              className={styles.flipCardImage}
+              src={props.img}
+              alt={props.title}
+            />
+          </div>
+          <div className={styles.flipCardBack}>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+          </div>
+        </div>
       </div>
     </Link>
   );
--- portfolio/src/screens/projets/Projets.jsx
+++ portfolio/src/screens/projets/Projets.jsx
@@ -13,30 +13,32 @@ export default function Projets() {
     <div className={styles.projets}>
       <Header />
       <h2>Projets</h2>
-      <ProjectCard
-        title="Bagarre Club"
-        img={BagarrePic}
-        description="Une appli de combat"
-        link="/projets/bagarre"
-      />
-      <ProjectCard
-        title="Loup Garou"
-        img={GarouPic}
-        description="Un générateur de cartes"
-        link="/projets/garou"
-      />
-      <ProjectCard
-        title="RunTime Terror"
-        img={RuntimePic}
-        description="Un jeu de rythme avec la webcam"
-        link="/projets/runtime"
-      />
-      <ProjectCard
-        title="Wild Advisor"
-        img={AdvisorPic}
-        description="Un guide cullinaire autour de l'école"
-        link="/projets/advisor"
-      />
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          title="Bagarre Club"
+          img={BagarrePic}
+          description="Une appli de combat"
+          link="/projets/bagarre"
+        />
+        <ProjectCard
+          title="Loup Garou"
+          img={GarouPic}
+          description="Un générateur de cartes"
+          link="/projets/garou"
+        />
+        <ProjectCard
+          title="RunTime Terror"
+          img={RuntimePic}
+          description="Un jeu de rythme avec la webcam"
+          link="/projets/runtime"
+        />
+        <ProjectCard
+          title="Wild Advisor"
+          img={AdvisorPic}
+          description="Un guide cullinaire autour de l'école"
+          link="/projets/advisor"
+        />
+      </div>
     </div>
   );
 }
