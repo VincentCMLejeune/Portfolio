9ac34ecd55aee42e79df819c9b9ebe2fa94fce44
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 20:59:26 +0100

    New routes for projects details

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,11 @@ import Contact from "./screens/contact/Contact";
 import Projets from "./screens/projets/Projets";
 import Home from "./screens/home/Home";
 
+import DetailAdvisor from "./screens/details projets/DetailAdvisor";
+import DetailBagarre from "./screens/details projets/DetailBagarre";
+import DetailGarou from "./screens/details projets/DetailGarou";
+import DetailRuntime from "./screens/details projets/DetailRuntime"
+
 // import styles from "./App.module.css";
 
 export default function App() {
@@ -15,6 +20,10 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projets" element={<Projets />} />
+          <Route path="/projets/advisor" element={<DetailAdvisor />} />
+          <Route path="/projets/bagarre" element={<DetailBagarre />} />
+          <Route path="/projets/garou" element={<DetailGarou />} />
+          <Route path="/projets/runtime" element={<DetailRuntime />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
