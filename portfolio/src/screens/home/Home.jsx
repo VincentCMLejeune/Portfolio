1ab4a4456f814cd2544956c4f00d904a77a79136
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 12:14:36 +0100

    Home screen with transition

--- portfolio/src/screens/home/Home.jsx
+++ portfolio/src/screens/home/Home.jsx
@@ -1,11 +1,71 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import profile from "../../assets/temp.png";
+
 import styles from "./Home.module.css";
 
 export default function Home() {
+  const [aboutHover, setAboutHover] = useState(false);
+  const [projetsHover, setProjetsHover] = useState(false);
+  const [contactHover, setContactHover] = useState(false);
+
   return (
-    <div>
-      <div className={styles.link}>Je suis Vincent</div>
-      <div className={styles.link}>développeur en formation</div>
-      <div className={styles.link}>Enchanté !</div>
+    <div className={styles.homeBackground}>
+      <div className={styles.linksContainer}>
+        <Link to="/about" style={{ textDecoration: "none" }}>
+          {aboutHover ? (
+            <div
+              className={styles.linkHover}
+              onMouseLeave={() => setAboutHover(false)}
+            >
+              me connaître
+            </div>
+          ) : (
+            <div
+              className={styles.link}
+              onMouseOver={() => setAboutHover(true)}
+            >
+              Je suis Vincent
+            </div>
+          )}
+        </Link>
+        <Link to="/projets" style={{ textDecoration: "none" }}>
+          {projetsHover ? (
+            <div
+              className={styles.linkHover}
+              onMouseLeave={() => setProjetsHover(false)}
+            >
+              voir mes projets
+            </div>
+          ) : (
+            <div
+              className={styles.link}
+              onMouseOver={() => setProjetsHover(true)}
+            >
+              développeur web
+            </div>
+          )}
+        </Link>
+        <Link to="/contact" style={{ textDecoration: "none" }}>
+          {contactHover ? (
+            <div
+              className={styles.linkHover}
+              onMouseLeave={() => setContactHover(false)}
+            >
+              me contacter
+            </div>
+          ) : (
+            <div
+              className={styles.link}
+              onMouseOver={() => setContactHover(true)}
+            >
+              enchanté
+            </div>
+          )}
+        </Link>
+      </div>
+      <img className={styles.profile} src={profile} alt="Profile" />
     </div>
   );
 }
