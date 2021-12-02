8d0ed596cb8a637c09f7e9daee14dbf5c1d694b5
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 22:35:45 +0100

    Styles about section

--- portfolio/src/screens/about/About.jsx
+++ portfolio/src/screens/about/About.jsx
@@ -5,15 +5,17 @@ import styles from "./About.module.css";
 
 export default function About() {
   return (
-    <div className={styles.about}>
+    <>
       <Header />
-      <div className={styles.introductionContainer}>
-        <div className={styles.introduction}>
-          <h2>A propos</h2>
+      <div className={styles.about}>
+        <div className={styles.introductionContainer}>
+          <div className={styles.introduction}>
+            <h2>A propos</h2>
+          </div>
         </div>
+        <Introduction />
+        <h2>Mes outils</h2>
       </div>
-      <Introduction />
-      <h2>Mes outils</h2>
-    </div>
+    </>
   );
 }
