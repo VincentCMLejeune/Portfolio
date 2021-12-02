aae067b10dee30abf675ec29ff45f66800bbcea5
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 15:51:35 +0100

    Progressed in the form, styling yet to be done

--- portfolio/src/components/form/Form.jsx
+++ portfolio/src/components/form/Form.jsx
@@ -3,10 +3,33 @@ import styles from "./Form.module.css";
 export default function Form() {
   return (
     <div className={styles.form}>
-      Je suis le form et je suis en forme. A mort.
-      <form action="https://formsubmit.co/9ca83417d36bcbc1302960454647c72c" method="POST">
-        <input type="text" name="name" required />
-        <input type="email" name="email" required />
+      <p className={styles.ensemble}><span id={styles.spin}></span> ensemble</p>
+      <form
+        action="https://formsubmit.co/9ca83417d36bcbc1302960454647c72c"
+        method="POST"
+      >
+        <label>
+          Nom (facultatif)
+          <input type="text" name="name" placeholder="Sarah" />
+        </label>
+        <label>
+          Mail
+          <input
+            type="email"
+            name="email"
+            placeholder="sarah.connor@skynet.com"
+            required
+          />
+        </label>
+        <label>
+          Message
+          <input
+            type="text"
+            name="message"
+            placeholder="Votre message"
+            required
+          />
+        </label>
         <button type="submit">Send</button>
       </form>
     </div>
