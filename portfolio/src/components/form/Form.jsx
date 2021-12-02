3d58e5463aaa67dca509c308a58ebf58dbbd2780
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 18:29:02 +0100

    Small fixes to form

--- portfolio/src/components/form/Form.jsx
+++ portfolio/src/components/form/Form.jsx
@@ -3,7 +3,6 @@ import styles from "./Form.module.css";
 export default function Form() {
   return (
     <div className={styles.form}>
-      <p className={styles.ensemble}><span id={styles.spin}></span> ensemble</p>
       <form
         action="https://formsubmit.co/9ca83417d36bcbc1302960454647c72c"
         method="POST"
@@ -30,7 +29,7 @@ export default function Form() {
             required
           />
         </label>
-        <button type="submit">Send</button>
+        <button type="submit">Envoyer</button>
       </form>
     </div>
   );
