f4cf1ba296b87797d133cae727f3d760cbc52703
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 18:31:27 +0100

    Added spining first word alongside ensemble

--- portfolio/src/screens/contact/Contact.jsx
+++ portfolio/src/screens/contact/Contact.jsx
@@ -6,6 +6,11 @@ import styles from "./Contact.module.css";
 export default function Contact() {
   return (
     <div className={styles.contact}>
+      <div className={styles.introContainer}>
+        <h2 className={styles.ensemble}>
+          <span id={styles.spin}></span> ensemble
+        </h2>
+      </div>
       <Header />
       <h2>Contact</h2>
       <Form />
