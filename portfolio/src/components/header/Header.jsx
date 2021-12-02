18435280f55945c1221750d6d75e6227fa6f0fee
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 14:39:34 +0100

    Basics of Header, pre underline tests

---
+++ portfolio/src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className={styles.name}>Vincent Lejeune</div>
+      </Link>
+      <div className={styles.links}>
+        <Link to="/about" style={{ textDecoration: "none" }}>
+          <div className={styles.link}>A propos</div>
+        </Link>
+        <Link to="/projets" style={{ textDecoration: "none" }}>
+          <div className={styles.link}>Projets</div>
+        </Link>
+        <Link to="/contact" style={{ textDecoration: "none" }}>
+          <div className={styles.link}>Contact</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- portfolio/src/screens/about/About.jsx
+++ portfolio/src/screens/about/About.jsx
@@ -1,8 +1,11 @@
+import Header from "../../components/header/Header"
+
 import styles from "./About.module.css"
 
 export default function About() {
     return (
         <div>
+            <Header />
             <h2 className={styles.about}>About</h2>
         </div>
     )
