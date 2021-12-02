46e3a9140c5811587d3781506537b1d63461ba1e
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-02 14:52:07 +0100

    Added header

--- portfolio/src/screens/contact/Contact.jsx
+++ portfolio/src/screens/contact/Contact.jsx
@@ -1,8 +1,11 @@
+import Header from "../../components/header/Header"
+
 import styles from "./Contact.module.css"
 
 export default function Contact() {
     return (
         <div>
+            <Header />
             <h2 className={styles.contact}>Contact</h2>
         </div>
     )
--- portfolio/src/screens/projets/Projets.jsx
+++ portfolio/src/screens/projets/Projets.jsx
@@ -1,8 +1,11 @@
+import Header from "../../components/header/Header"
+
 import styles from "./Projets.module.css"
 
 export default function Projets() {
     return (
         <div>
+            <Header />
             <h2 className={styles.projets}>Projets</h2>
         </div>
     )
