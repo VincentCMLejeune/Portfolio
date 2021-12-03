9c565197d20ac38cd53088f96f412a1f6ced15ed
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-03 20:36:53 +0100

    Wrote first version of garou project content

--- portfolio/src/screens/details projets/DetailGarou.jsx
+++ portfolio/src/screens/details projets/DetailGarou.jsx
@@ -1,22 +1,152 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import Header from "../../components/header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+import ContactScreen from "../../assets/contactscreen.png";
+import ContactScreenHover from "../../assets/contactscreenhover.png";
+
+import Advisor from "../../assets/advisorscreen.png";
+import Bagarre from "../../assets/bagarrescreen.png";
+import Runtime from "../../assets/runtimescreen.png";
+
+import s1 from "../../assets/garou screens/1.png";
+import s4 from "../../assets/garou screens/4.png";
+import s6 from "../../assets/garou screens/6.png";
 
 import styles from "./Details.module.css";
 
 export default function DetailGarou() {
+  const [hoverContact, setHoverContact] = useState(false);
   return (
     <div>
       <Header />
+      <Sidebar />
+
+      <div className={styles.titleContainer}>
+        <h2 className={styles.projectsTitle}>Garou</h2>
+      </div>
+
+      <div className={styles.apercus}>
+        <img src={s4} alt="Screenshot" />
+      </div>
+
       <div className={styles.container}>
-        <h2 className={styles.title}>La carte du Loup Garou</h2>
+        <h3>Objectifs</h3>
+        <p>
+          Ayant fait 6 ans de théâtre, les{" "}
+          <a
+            href="https://www.loups-garous-en-ligne.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Loups-Garous de Thiercelieux
+          </a>{" "}
+          sont parmi mes jeux préférés. Mais mon jeu de cartes étant chez mes
+          parents, j'ai saisi l'occasion de faire un projet entièrement
+          personnel. Cette application accueille l'utilisateur en demandant le
+          nombre de villageois, garous et quels personnages spéciaux sont
+          souhaités (voyante, sorcière, etc.). En retour, et après avoir
+          enregistré les noms des joueurs, il distribue aléatoirement les
+          cartes, qui peuvent être retournées, révélant au public la véritable
+          identité du joueur !
+        </p>
+
+        <h3>Outils</h3>
+        <div className={styles.outilsContainer}>
+          <img
+            src="https://img.shields.io/badge/react%20-%2320232a.svg?&style=for-the-badge&logo=react&logoColor=%2361DAFB"
+            alt="react"
+          />
+          <img
+            src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
+            alt="Css"
+          />
+          <img
+            src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"
+            alt="Javascript"
+          />
+          <img
+            src="https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white"
+            alt="npm"
+          />
+          <img
+            src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white"
+            alt="Node"
+          />
+          <img
+            src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white"
+            alt="Git"
+          />
+        </div>
+      </div>
+
+      <div className={styles.apercusLarge}>
+        <img src={s6} alt="Screenshot" />
+      </div>
+
+      <div className={styles.container}>
+        <h3>Défis et problèmes rencontrés</h3>
+        <h4>Organiser un formulaire</h4>
+        <p>
+          Noter les noms et distribuer des cartes. Simple sur le papier,
+          n'est-ce pas ? Mais le coder m'a procuré bien des défis (empêcher deux
+          noms identiques, distribuer aléatoirement les cartes).
+        </p>
+        <h4>Hébergement en ligne</h4>
+        <p>
+          Ce projet est toujours en cours de développement. Mon but est de
+          permettre d'héberger des parties en ligne, avec pour chaque joueur des
+          droits différents (il ne s'agit pas d'autoriser tout le monde à
+          retourner les cartes des autres !).
+        </p>
+      </div>
+
+      <div className={styles.apercusLarge}>
+        <img src={s1} alt="Screenshot" />
+      </div>
+
+      <div className={styles.container}>
+        <h3>Leçons</h3>
+        <h4>L'expérience utilisateur d'abord</h4>
+        <p>
+          Ce site doit donner envie de lancer une partie de loup garou. Problème
+          : tout le monde n'a pas le même rapport à ce jeu, mes deux user
+          stories principales étant le joueur chevronné, qui connaît les règles
+          par coeur, et le nouveau venu qui a besoin d'être guidé. Il faut faire
+          cohabiter les deux ensemble, tout en rendant le site le plus
+          visuellement agréable possible.
+        </p>
+      </div>
+
+      <div className={styles.ohterProjects}>
+        <h3>Mes autres projets</h3>
+        <div className={styles.otherProjectsContainer}>
+          <Link to="/projets/advisor">
+            <img src={Advisor} alt="Wild Advisor" />
+          </Link>
+          <Link to="/projets/runtime">
+            <img src={Runtime} alt="Runtime Terror" />
+          </Link>
+          <Link to="/projets/bagarre">
+            <img src={Bagarre} alt="Bagarre Club" />
+          </Link>
+        </div>
+      </div>
+      <div
+        className={styles.callToAction}
+        onMouseOver={() => setHoverContact(true)}
+        onMouseLeave={() => setHoverContact(false)}
+      >
+        <Link to="/contact">
+          {hoverContact ? (
+            <img src={ContactScreen} alt="Computer" />
+          ) : (
+            <img src={ContactScreenHover} alt="Computer" />
+          )}
+        </Link>
       </div>
     </div>
   );
 }
-
-
-
-// Loup garou
-// Un de mes jeux préférés, et une possibilité d'entraînement, joindre l'utile à l'agréable dans un projet totalement personnel
-// React (c'est tout ?!)
-// Difficultés : parcours de navigation et asynchrone. Se servir de composants react et les faire travailler dans l'ordre est très puissant, mais je me suis pris quelques chausses trappes sur les propriétés à passer entre pages (enregistrer les passages, données, etc.) le plus ux possible. Problème, il y a beaucoup dinfos et des personnes assez différentes. Le rendre disponible en ligne c'est toujours un travail en cours
-// Lessons learned, cf AU ddssus
