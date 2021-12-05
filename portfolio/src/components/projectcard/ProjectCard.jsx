6293165b062384b4c1b59770308da7d1ca487e0b
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-05 11:37:54 +0100

    Style fix

--- portfolio/src/components/projectcard/ProjectCard.jsx
+++ portfolio/src/components/projectcard/ProjectCard.jsx
@@ -15,7 +15,7 @@ export default function ProjectCard(props) {
             }}
           ></div>
           <div className={styles.flipCardBack}>
-            <h2>{props.title}</h2>
+            <h2 className={styles.cardTitle}>{props.title}</h2>
             <p className={styles.subtitle}>{props.description}</p>
             <p className={styles.prompt}>Cliquez pour en savoir plus</p>
           </div>
