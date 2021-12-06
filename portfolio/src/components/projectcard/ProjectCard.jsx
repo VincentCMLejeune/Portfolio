2bcb501bdb0ff1dc2a17602273b1762e516592b1
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-12-06 20:00:39 +0100

    Restyled the components to be chrome compatible

--- portfolio/src/components/projectcard/ProjectCard.jsx
+++ portfolio/src/components/projectcard/ProjectCard.jsx
@@ -7,13 +7,9 @@ export default function ProjectCard(props) {
     <Link to={props.link} style={{ textDecoration: "none" }}>
       <div className={styles.flipCard}>
         <div className={styles.flipCardInner}>
-          <div
-            className={styles.flipCardFront}
-            style={{
-              background: `url(${props.img}) no-repeat center center fixed`,
-              backgroundSize: "cover",
-            }}
-          ></div>
+          <div className={styles.flipCardFront}>
+            <img src={props.img} alt={props.title} />
+          </div>
           <div className={styles.flipCardBack}>
             <h2 className={styles.cardTitle}>{props.title}</h2>
             <p className={styles.subtitle}>{props.description}</p>
